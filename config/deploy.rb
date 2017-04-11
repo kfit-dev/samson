c17bb41409e42017abbffc088a5abd1bb4edea34
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2017-04-11 15:30:15 +0800

    Link .env

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :rbenv_ruby, '2.3.3'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
